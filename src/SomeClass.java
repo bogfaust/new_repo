9d00c48070b49f09792969f7fa8fb0368846fd61
bogfaust <73497633+bogfaust@users.noreply.github.com>
2023-03-02 01:54:19 +0400

    Update SomeClass.java

--- src/SomeClass.java
+++ src/SomeClass.java
@@ -1,5 +1,14 @@
+import java.io.*;
+
 public class SomeClass {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.print("Hello, World!");
+
+		String str = "Hello";
+		FileOutputStream outputStream = new FileOutputStream("1.txt");
+		byte[] strToBytes = str.getBytes();
+		outputStream.write(strToBytes);
+
+		outputStream.close();
 	}
 }
